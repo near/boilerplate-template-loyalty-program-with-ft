459399eea8d9362ae46995cda7eaa5bf97cb1ca8
Milena Krawczyk <mmilena.krawczyk@gmail.com>
2022-11-17 09:26:00 +0100

    Logged-in view

--- frontend/App.js
+++ frontend/App.js
@@ -3,12 +3,17 @@ import React from 'react';
 
 import './assets/global.css';
 
-import { SignInPrompt, SignOutButton } from './ui-components';
+import { SignOutButton } from './ui-components';
 
 export default function App({ isSignedIn, helloNEAR, wallet }) {
   const [valueFromBlockchain, setValueFromBlockchain] = React.useState();
+  const [isInitialized, setIsInitialized] = React.useState(false);
 
   const [uiPleaseWait, setUiPleaseWait] = React.useState(true);
+
+  const [name, setName] = React.useState();
+  const [symbol, setSymbol] = React.useState();
+  const [totalSupply, setTotalSupply] = React.useState();
 
   // Get blockchian state once on component load
   React.useEffect(() => {
@@ -22,46 +27,70 @@ export default function App({ isSignedIn, helloNEAR, wallet }) {
   }, []);
 
 
-  function changeGreeting(e) {
-    e.preventDefault();
-    setUiPleaseWait(true);
-    const { greetingInput } = e.target.elements;
-    helloNEAR
-      .setGreeting(greetingInput.value)
-      .then(async () => {
-        return helloNEAR.getGreeting();
-      })
-      .then(setValueFromBlockchain)
-      .finally(() => {
-        setUiPleaseWait(false);
-      });
-  }
+  // function changeGreeting(e) {
+  //   e.preventDefault();
+  //   setUiPleaseWait(true);
+  //   const { greetingInput } = e.target.elements;
+  //   helloNEAR
+  //     .setGreeting(greetingInput.value)
+  //     .then(async () => {
+  //       return helloNEAR.getGreeting();
+  //     })
+  //     .then(setValueFromBlockchain)
+  //     .finally(() => {
+  //       setUiPleaseWait(false);
+  //     });
+  // }
 
   function createLoayltyToken(e) {
     e.preventDefault();
     console.log("Loyalty Token Created!");
+    setIsInitialized(true);
   }
 
   return (
+    
     <>
-      {isSignedIn &&
-        <SignOutButton accountId={wallet.accountId} onClick={() => wallet.signOut()} />
-      }
-      <main className={uiPleaseWait ? 'please-wait' : ''}>
-        <h1>
-          Merchant view
-        </h1>
-        <form onSubmit={createLoayltyToken} className="change">
-          <input autoComplete="off" id="name" placeholder="Name" />
-          <input autoComplete="off" id="symbol" placeholder="Symbol" />
-          <button className="walletButton" disabled={isSignedIn} onClick={() => wallet.signIn()}>{isSignedIn ? wallet.accountId : "Sign in with Near wallet"}</button>
-          <input type="number" autoComplete="off" placeholder="Total supply" id="totalSupply" />
-          <button>
-            <span>Create Loyalty Token</span>
-            <div className="loader"></div>
-          </button>
-        </form>
-      </main>
+    {!isInitialized &&
+      <>
+        {isSignedIn &&
+          <SignOutButton accountId={wallet.accountId} onClick={() => wallet.signOut()} />
+        }
+        <main className={uiPleaseWait ? 'please-wait' : ''}>
+          <h1>
+            Merchant view
+          </h1>
+          <form onSubmit={createLoayltyToken} className="change">
+            <input autoComplete="off" id="name" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" />
+            <input autoComplete="off" id="symbol" value={symbol} onChange={(e) => setSymbol(e.target.value)} placeholder="Symbol" />
+            <button className="walletButton" disabled={isSignedIn} onClick={() => wallet.signIn()}>{isSignedIn ? wallet.accountId : "Sign in with Near wallet"}</button>
+            <input type="number" autoComplete="off" id="totalSupply" value={totalSupply} onChange={(e) => setTotalSupply(e.target.value)} placeholder="Total supply"/>
+            <button>
+              <span>Create Loyalty Token</span>
+              <div className="loader"></div>
+            </button>
+          </form>
+        </main>
+      </>
+    }
+    {isInitialized &&
+      <>
+        <main className={uiPleaseWait ? 'please-wait' : 'ftDetailsWrapper'}>
+          <h1>
+            Merchant view
+          </h1>
+          <div className='row'>
+            <div>Loyalty coin name</div>
+            <div>Logged-in wallet</div>
+          </div>
+          <div className="ftDetailsWrapper">
+            <div className='ftDetails'>Name</div>
+            <div className='ftDetails'>Symbol</div>
+            <div className='ftDetails'>Total supply</div>
+          </div>
+        </main>
+      </>
+    }
     </>
   );
 }
--- frontend/ui-components.js
+++ frontend/ui-components.js
@@ -1,28 +1,5 @@
 import React from 'react';
 
-export function SignInPrompt({ greeting, onClick }) {
-  return (
-    <main>
-      <h1>
-        The contract says: <span className="greeting">{greeting}</span>
-      </h1>
-      <h3>Welcome to NEAR!</h3>
-      <p>
-        Your contract is storing a greeting message in the NEAR blockchain. To change it you need to sign in using the
-        NEAR Wallet. It is very simple, just use the button below.
-      </p>
-      <p>
-        Do not worry, this app runs in the test network ("testnet"). It works just like the main network ("mainnet"),
-        but using NEAR Tokens that are only for testing!
-      </p>
-      <br />
-      <p style={{ textAlign: 'center' }}>
-        <button onClick={onClick}>Sign in with NEAR Wallet</button>
-      </p>
-    </main>
-  );
-}
-
 export function SignOutButton({ accountId, onClick }) {
   return (
     <button style={{ float: 'right' }} onClick={onClick}>

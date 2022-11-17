adce2009e7a8ebb736700c9e82b130edaf58243e
Milena Krawczyk <mmilena.krawczyk@gmail.com>
2022-11-17 12:23:00 +0100

    Creating ft pool

--- frontend/near-interface.js
+++ frontend/near-interface.js
@@ -1,20 +1,30 @@
 /* Talking with a contract often involves transforming data, we recommend you to encapsulate that logic into a class */
 
-export class HelloNEAR {
+export class LoyaltyProgramWithFtContractInterface {
   constructor({ contractId, walletToUse }) {
     this.contractId = contractId;
     this.wallet = walletToUse;
   }
 
-  async getGreeting() {
-    return await this.wallet.viewMethod({ contractId: this.contractId, method: 'get_greeting' });
-  }
-
-  async setGreeting(greeting) {
+  async createFungibleTokenPool(name, symbol, totalSupply) {
+    console.log("CONTRACTID: " + this.contractId);
     return await this.wallet.callMethod({
       contractId: this.contractId,
-      method: 'set_greeting',
-      args: { message: greeting },
-    });
+      method: 'new_fungible_token_pool',
+      args: { owner_id: this.wallet.accountId,  name, symbol, total_supply: totalSupply },
+    })
   }
+
+  // async getGreeting() {
+  //   return await this.wallet.viewMethod({ contractId: this.contractId, method: 'get_greeting' });
+  // }
+
+  // async setGreeting(greeting) {
+  //   return await this.wallet.callMethod({
+  //     contractId: this.contractId,
+  //     method: 'set_greeting',
+  //     args: { message: greeting },
+  //   });
+  // }
+
 }

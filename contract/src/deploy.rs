48a5769a50c8a59ad298f5ce2e1815d2242eddbe
gagdiez <gagdiez.c@gmail.com>
2022-11-23 12:10:09 -0300

    Fixed Factory

--- contract/src/deploy.rs
+++ contract/src/deploy.rs
@@ -1,16 +1,16 @@
+use near_sdk::json_types::U128;
 use near_sdk::serde::Serialize;
 use near_sdk::{env, log, near_bindgen, AccountId, Balance, Promise, PromiseError, PublicKey};
-use near_sdk::json_types::U128;
 
-use crate::{Contract, ContractExt, NEAR_PER_STORAGE, NO_DEPOSIT, TGAS};
+use crate::{Contract, ContractExt, NEAR_PER_STORAGE, NO_DEPOSIT, TGAS, FT_CONTRACT};
 
 #[derive(Serialize)]
 #[serde(crate = "near_sdk::serde")]
 struct FungibleTokenInitArgs {
-    owner_id: AccountId, 
-    name: String, 
-    symbol: String, 
-    total_supply: U128
+    owner_id: AccountId,
+    name: String,
+    symbol: String,
+    total_supply: U128,
 }
 
 #[near_bindgen]
@@ -20,8 +20,8 @@ impl Contract {
         &mut self,
         name: String,
         ft_owner_id: AccountId,
-        token_name: String, 
-        token_symbol: String, 
+        token_name: String,
+        token_symbol: String,
         token_total_supply: U128,
         public_key: Option<PublicKey>,
     ) -> Promise {
@@ -36,20 +36,31 @@ impl Contract {
         // Assert enough money is attached to create the account and deploy the contract
         let attached = env::attached_deposit();
 
-        let contract_bytes = self.code.len() as u128;
+        let contract_bytes = FT_CONTRACT.len() as u128;
         let minimum_needed = NEAR_PER_STORAGE * contract_bytes;
         assert!(
             attached >= minimum_needed,
             "Attach at least {minimum_needed} yⓃ"
         );
 
-        let init_args = near_sdk::serde_json::to_vec(&FungibleTokenInitArgs { owner_id: ft_owner_id, name: token_name, symbol: token_symbol, total_supply: token_total_supply }).unwrap();
+        let init_args = near_sdk::serde_json::to_vec(&FungibleTokenInitArgs {
+            owner_id: ft_owner_id,
+            name: token_name,
+            symbol: token_symbol,
+            total_supply: token_total_supply,
+        })
+        .unwrap();
 
         let mut promise = Promise::new(subaccount.clone())
             .create_account()
             .transfer(attached)
-            .deploy_contract(self.code.clone())
-            .function_call("new_fungible_token_pool".to_owned(), init_args, NO_DEPOSIT, TGAS * 300);
+            .deploy_contract(FT_CONTRACT.to_vec())
+            .function_call(
+                "new_fungible_token_pool".to_owned(),
+                init_args,
+                NO_DEPOSIT,
+                TGAS * 20,
+            );
 
         // Add full access key is the user passes one
         if let Some(pk) = public_key {
@@ -58,11 +69,13 @@ impl Contract {
 
         // Add callback
         promise.then(
-            Self::ext(env::current_account_id()).create_factory_subaccount_and_deploy_callback(
-                subaccount,
-                env::predecessor_account_id(),
-                attached,
-            ),
+            Self::ext(env::current_account_id())
+                .with_static_gas(TGAS * 5)
+                .create_factory_subaccount_and_deploy_callback(
+                    subaccount,
+                    env::predecessor_account_id(),
+                    attached,
+                ),
         )
     }
 

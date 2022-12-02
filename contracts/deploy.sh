3e41dcec7dc022057f40040446c5a1e6ed113094
Milena Krawczyk <mmilena.krawczyk@gmail.com>
2022-12-02 14:56:38 +0100

    Fixes

--- contracts/deploy.sh
+++ contracts/deploy.sh
@@ -11,3 +11,5 @@ echo ">> Deploying contract"
 
 # https://docs.near.org/tools/near-cli#near-dev-deploy
 near dev-deploy --wasmFile ./target/wasm32-unknown-unknown/release/reward_factory.wasm
+
+cp neardev/dev-account.env ../frontend/.env

d31480197f42c38664da1242db6627784e78249b
marcinbodnar <me@marcinbodnar.com>
2023-01-23 20:48:32 +0100

    Create SignedOut component

---
+++ frontend/modules/MarchantView/SignedOut/index.js
@@ -0,0 +1,11 @@
+import LoyaltyPrograms from '../../CustomerView/ProgramNotActive/LoyaltyPrograms';
+
+const SignedOut = () => {
+  return (
+    <div>
+      <LoyaltyPrograms />
+    </div>
+  );
+};
+
+export default SignedOut;

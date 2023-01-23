ee1e71b726a23b841b4e0cbd0e12fcdd5a245363
marcinbodnar <me@marcinbodnar.com>
2023-01-23 20:51:03 +0100

    Create ListOfTx component

---
+++ frontend/modules/CustomerView/ProgramIsActive/ListOfTx.js
@@ -0,0 +1,26 @@
+const ListOfTx = () => {
+  return (
+    <>
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      TODO: list of purchases
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </>
+  );
+};
+
+export default ListOfTx;

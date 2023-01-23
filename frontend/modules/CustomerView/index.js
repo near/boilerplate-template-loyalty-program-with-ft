3b0ae13c06962a975818a7233dcee129f193f2dc
marcinbodnar <me@marcinbodnar.com>
2023-01-23 20:53:38 +0100

    Create CustomerView component

--- frontend/modules/CustomerView/index.js
+++ frontend/modules/CustomerView/index.js
@@ -1,25 +1,103 @@
-const CustomerView = ({ uiPleaseWait, customerUuid, customerBalance, purchaseWithCC, purchaseWithTokens }) => {
-  return (
-    <div className={uiPleaseWait ? 'please-wait' : 'container'}>
-      <h1>Customer view</h1>
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getCustomerPrefix } from '../../utils/utils';
+import Header from './Header';
+import ProgramIsActive from './ProgramIsActive';
+import ProgramNotActive from './ProgramNotActive';
+import Welcome from './Welcome';
 
-      <div className="change">
-        <div>Your ID number: {customerUuid}</div>
-        <div>Your tokens balance: {customerBalance}</div>
-        <span className="ftDetails"> 1 Large Coffee </span>
-        <span className="ftDetails"> 3$ </span>
-        <span className="ftDetails"> 30 Tokens </span>
-        <hr />
-        <button className="btn btn-primary" onClick={purchaseWithCC}>
-          <span>Purchase with CC</span>
-          <div className="loader"></div>
-        </button>
-        <button className="btn btn-primary" onClick={purchaseWithTokens}>
-          <span>Purchase with Tokens</span>
-          <div className="loader"></div>
-        </button>
-      </div>
-    </div>
+const CustomerView = ({ factory, wallet, customer }) => {
+  const { merchantAddress } = useParams();
+
+  const [programExists, setProgramExists] = useState(false);
+  const [ftMetadata, setFtMetadata] = useState({});
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [customerBalance, setCustomerBalance] = useState();
+  const [customerUuid, setCustomerUuid] = useState('');
+
+  function purchaseWithCC(e) {
+    customer
+      .purchaseCoffeeWithCC()
+      .then(() => alert('Coffee bought with Credit Card'))
+      .then(() => customer.getBalance().then((b) => setCustomerBalance(b)))
+      .catch(alert)
+      .catch(alert);
+  }
+
+  async function purchaseWithTokens(e) {
+    customer
+      .purchaseCoffeeWithTokens()
+      .then(() => alert('Coffee bought with tokens'))
+      .then(() => customer.getBalance().then((b) => setCustomerBalance(b)))
+      .catch(alert)
+      .catch(alert);
+  }
+
+  useEffect(() => {
+    const checkSignIn = async () => {
+      const isSignedIn = await wallet.startUp();
+
+      setIsSignedIn(isSignedIn);
+    };
+
+    checkSignIn();
+  }, [wallet]);
+
+  useEffect(() => {
+    if (!isSignedIn || !merchantAddress) {
+      return;
+    }
+
+    setCustomerUuid(getCustomerPrefix());
+    factory.checkProgramExists(merchantAddress).then((programExists) => {
+      setProgramExists(programExists);
+    });
+    // .finally(() => setUiPleaseWait(false));
+  }, [factory, isSignedIn, merchantAddress]);
+
+  useEffect(() => {
+    if (!programExists) {
+      return;
+    }
+
+    //   // setUiPleaseWait(true);
+    factory
+      .getProgram(merchantAddress)
+      .then((metadata) => {
+        setFtMetadata(metadata.ft);
+      })
+      .then(() => customer.getBalance().then((b) => setCustomerBalance(b)))
+      .catch(alert);
+  }, [customer, factory, merchantAddress, programExists]);
+
+  const product = {
+    name: 'Large Coffe',
+    fiatCost: '1$',
+    tokenCost: 30,
+    tokensPerUnit: 10,
+  };
+
+  const canCollect = customerBalance >= product.tokenCost;
+
+  const programIsActive = !!ftMetadata.account_id;
+
+  return (
+    <>
+      <Header />
+      <Welcome ftMetadata={ftMetadata} product={product} programIsActive={programIsActive} />
+      {programIsActive && (
+        <ProgramIsActive
+          ftMetadata={ftMetadata}
+          product={product}
+          customerUuid={customerUuid}
+          customerBalance={customerBalance}
+          purchaseWithCC={purchaseWithCC}
+          canCollect={canCollect}
+          purchaseWithTokens={purchaseWithTokens}
+        />
+      )}
+      {programIsActive || <ProgramNotActive merchantAddress={merchantAddress} />}
+    </>
   );
 };
 
